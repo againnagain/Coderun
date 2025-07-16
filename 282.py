c17dc5a026c959f5bf3e219c4a5ff4ffe428e668
vladimir_bogatyrev <anavrinvova@gmail.com>
2025-07-16 22:16:48 +0300

    Added 282.py task solution

--- 282.py
+++ 282.py
@@ -2,3 +2,26 @@
 282. D-CoV-3999
 Difficulty: Easy
 '''
+
+n = int(input())
+
+employees_tests = list(map(int, input().split()))
+meetings_employees = dict()
+
+for employee_id in range(n):
+    meetings = list(map(int, input().split()))[1:]
+
+    for meeting in meetings:
+        if meetings_employees.get(meeting) != None:
+            meetings_employees[meeting] = meetings_employees.get(meeting) + [employee_id]
+        else:
+            meetings_employees[meeting] = [employee_id]
+
+for meeting_id in sorted(meetings_employees.keys()):
+    employees = meetings_employees.get(meeting_id)
+    for employee_id in employees:
+        if employees_tests[employee_id] == 1:
+            for employee_id in employees:
+                employees_tests[employee_id] = 1
+
+print(' '.join(list(map(str, employees_tests))))
